96032d00369493da9f9ab9a79784f47fe246701e
flywind <xzsflywind@gmail.com>
2020-01-01 13:46:40 +0800

    improve

--- cache/cache.nim
+++ cache/cache.nim
@@ -17,16 +17,16 @@ type
     info: CachedInfo
 
 
-proc initLruCached*[A, B](maxSize: Natural = 128): LRUCached[A, B] =
+proc initLruCached*[A, B](maxSize: Natural = 128): LRUCached[A, B] {.inline.} =
   LRUCached[A, B](map: initTable[A, MapValue[A, B]](),
       cached: initDoublyLinkedList[KeyPair[A, B]](), info: (hits: 0,
           misses: 0, maxSize: maxSize))
 
-proc moveToFront*[A, B](x: var LRUCached[A, B], node: MapValue[A, B]) =
+proc moveToFront*[A, B](x: var LRUCached[A, B], node: MapValue[A, B]) {.inline.} =
   x.cached.remove(node)
   x.cached.prepend(node)
 
-proc get*[A, B](x: var LRUCached[A, B], key: A): B =
+proc get*[A, B](x: var LRUCached[A, B], key: A): B {.inline.} =
   if key in x.map:
     x.info.hits += 1
     let node = x.map[key]
@@ -34,7 +34,7 @@ proc get*[A, B](x: var LRUCached[A, B], key: A): B =
     return node.value.valuePart
   x.info.misses += 1
 
-proc put*[A, B](x: var LRUCached[A, B], key: A, value: B) =
+proc put*[A, B](x: var LRUCached[A, B], key: A, value: B) {.inline.} =
   if key in x.map:
     x.info.hits += 1
     var node = x.map[key]
@@ -50,30 +50,11 @@ proc put*[A, B](x: var LRUCached[A, B], key: A, value: B) =
   x.map[key] = node
   moveToFront(x, node)
 
-proc `[]`*[A, B](x: var LRUCached[A, B], key: A): B =
-  if key in x.map:
-    x.info.hits += 1
-    let node = x.map[key]
-    moveToFront(x, node)
-    return node.value.valuePart
-  x.info.misses += 1
+proc `[]`*[A, B](x: var LRUCached[A, B], key: A): B {.inline.} =
+  x.get(key)
 
-proc `[]=`*[A, B](x: var LRUCached[A, B], key: A, value: B) =
-  if key in x.map:
-    x.info.hits += 1
-    var node = x.map[key]
-    node.value.valuePart = value
-    moveToFront(x, node)
-    return
-  x.info.misses += 1
-  if x.map.len >= x.info.maxSize:
-    let node = x.cached.tail
-    x.cached.remove(node)
-    x.map.del(node.value.keyPart)
-
-  let node = newDoublyLinkedNode((keyPart: key, valuePart: value))
-  x.map[key] = node
-  moveToFront(x, node)
+proc `[]=`*[A, B](x: var LRUCached[A, B], key: A, value: B) {.inline.} =
+  x.put(key, value)
 
 proc contains*[A, B](x: var LRUCached[A, B], key: A): bool =
   if key in x.map:
